598871dd0dca4a64886e66396087c0e40ac9c7dc
Shane Becker <veganstraightedge@gmail.com>
2020-03-29 15:06:42 -0700

    Quotes

--- lib/faster_factory/cli.rb
+++ lib/faster_factory/cli.rb
@@ -5,31 +5,31 @@ Options = Struct.new(:name)
 module FasterFactory
   class CLI
     def self.start options
-      args = Options.new("world")
+      args = Options.new('world')
 
       opt_parser = OptionParser.new do |opts|
         opts.banner = 'Usage: faster_factory [command] [path/to/folder/or/file] [options]'
 
-        opts.on("--dry-run", "Prints report instead of keeping any changes") do
+        opts.on('--dry-run', 'Prints report instead of keeping any changes') do
           puts opts
           exit
         end
 
-        opts.on("--no-git", "Runs without committing successful changes") do
+        opts.on('--no-git', 'Runs without committing successful changes') do
           puts opts
           exit
         end
 
-        opts.on("help", "-h", "--help", "Displays this help") do
+        opts.on('help', '-h', '--help', 'Displays this help') do
           puts opts
           exit
         end
 
-        opts.on("-mMESSAGE", "--message=MESSAGE", "Custom commit message, can use {{file}} for file name") do |m|
+        opts.on('-mMESSAGE', '--message=MESSAGE', 'Custom commit message, can use {{file}} for file name') do |m|
           args.message = m
         end
 
-        opts.on("-sSTRATEGY", "--strategy=STRATEGY", "Test strategy: line file all line,file file,all line,file,all") do |s|
+        opts.on('-sSTRATEGY', '--strategy=STRATEGY', 'Test strategy: line file all line,file file,all line,file,all') do |s|
           args.strategy = s
         end
       end

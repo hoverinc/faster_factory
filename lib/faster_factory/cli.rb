514b36da9be3ac51719d0932922b6ce1b23dfbdb
Shane Becker <veganstraightedge@gmail.com>
2020-03-29 16:10:50 -0700

    Add type casting to CLI args

--- lib/faster_factory/cli.rb
+++ lib/faster_factory/cli.rb
@@ -8,11 +8,11 @@ module FasterFactory
       option_parser = OptionParser.new do |opts|
         opts.banner = 'Usage: faster_factory [command] [path/to/folder/or/file] [options]'
 
-        opts.on('--dry-run', 'Prints report instead of keeping any changes') do |dr|
+        opts.on('--dry-run', TrueClass, 'Prints report instead of keeping any changes') do |dr|
           args.dry_run = dr
         end
 
-        opts.on('--no-git', 'Runs without committing successful changes') do |ng|
+        opts.on('--no-git', TrueClass, 'Runs without committing successful changes') do |ng|
           args.no_git = ng
         end
 
@@ -21,17 +21,24 @@ module FasterFactory
           exit
         end
 
-        opts.on('-mMESSAGE', '--message=MESSAGE', 'Sets a custom message for each git commit', 'It can include {{file}} for file name') do |m|
+        opts.on('-mMESSAGE', '--message=MESSAGE', String, 'Sets a custom message for each git commit', 'It can include {{file}} for file name') do |m|
           args.message = m
         end
 
-        opts.on('-sSTRATEGY', '--strategy=STRATEGY', 'Sets the test strategy: line|file|all', 'You can combine strategies for extra checks:', 'line,file|line,all|file,all|line,file,all') do |s|
+        opts.on('-sSTRATEGY', '--strategy=STRATEGY', Array, 'Sets the test strategy: line|file|all', 'You can combine strategies for extra checks:', 'line,file|line,all|file,all|line,file,all') do |s|
           args.strategy = s
         end
       end
 
       option_parser.parse! options
-      args
+      puts args.dry_run.inspect
+      puts args.dry_run.class
+      puts args.no_git.inspect
+      puts args.no_git.class
+      puts args.message.inspect
+      puts args.message.class
+      puts args.strategy.inspect
+      puts args.strategy.class
     end
   end
 end

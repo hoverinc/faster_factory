dd418acb860e80aef510de1858453c6943fc0bed
Shane Becker <veganstraightedge@gmail.com>
2020-03-29 17:03:08 -0700

    Extract methods to FasterFactory::Options object

--- lib/faster_factory/cli.rb
+++ lib/faster_factory/cli.rb
@@ -2,43 +2,13 @@ require 'optparse'
 
 module FasterFactory
   class CLI
-    def self.start options
-      args = FasterFactory::Options.new
+    class << self
+      def start options
+        options = FasterFactory::Options.parse options
+        # commands = FasterFactory::Commands.parse(options)
 
-      option_parser = OptionParser.new do |opts|
-        opts.banner = 'Usage: faster_factory [command] [path/to/folder/or/file] [options]'
-
-        opts.on('--dry-run', TrueClass, 'Prints report instead of keeping any changes') do |dr|
-          args.dry_run = dr
-        end
-
-        opts.on('--no-git', TrueClass, 'Runs without committing successful changes') do |ng|
-          args.no_git = ng
-        end
-
-        opts.on('-h', '--help', 'Displays this help text') do
-          puts opts
-          exit
-        end
-
-        opts.on('-mMESSAGE', '--message=MESSAGE', String, 'Sets a custom message for each git commit', 'It can include {{file}} for file name') do |m|
-          args.message = m
-        end
-
-        opts.on('-sSTRATEGY', '--strategy=STRATEGY', Array, 'Sets the test strategy: line|file|all', 'You can combine strategies for extra checks:', 'line,file|line,all|file,all|line,file,all') do |s|
-          args.strategy = s
-        end
+        puts options.inspect
       end
-
-      option_parser.parse! options
-      puts args.dry_run.inspect
-      puts args.dry_run.class
-      puts args.no_git.inspect
-      puts args.no_git.class
-      puts args.message.inspect
-      puts args.message.class
-      puts args.strategy.inspect
-      puts args.strategy.class
     end
   end
 end
--- lib/faster_factory/options.rb
+++ lib/faster_factory/options.rb
@@ -1,5 +1,45 @@
 module FasterFactory
   class Options
-    attr_accessor :name, :dry_run, :no_git, :message, :strategy
+    attr_accessor :dry_run, :no_git, :message, :strategy
+
+    class << self
+      def parse options
+        FasterFactory::Options.new options
+      end
+    end
+
+    def initialize options
+      option_parser.parse! options
+      self
+    end
+
+    private
+
+    def option_parser
+      OptionParser.new do |opts|
+        opts.banner = 'Usage: faster_factory [command] [path/to/folder/or/file] [options]'
+
+        opts.on('--dry-run', TrueClass, 'Prints report instead of keeping any changes') do |dr|
+          @dry_run = dr
+        end
+
+        opts.on('--no-git', TrueClass, 'Runs without committing successful changes') do |ng|
+          @no_git = ng
+        end
+
+        opts.on('-h', '--help', 'Displays this help text') do
+          puts opts
+          exit
+        end
+
+        opts.on('-mMESSAGE', '--message=MESSAGE', String, 'Sets a custom message for each git commit', 'It can include {{file}} for file name') do |m|
+          @message = m
+        end
+
+        opts.on('-sSTRATEGY', '--strategy=STRATEGY', Array, 'Sets the test strategy: line|file|all', 'You can combine strategies for extra checks:', 'line,file|line,all|file,all|line,file,all') do |s|
+          @strategy = s
+        end
+      end
+    end
   end
 end

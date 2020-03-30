aa05dea42287a6b4fd98276de4a50aba23056e98
Shane Becker <veganstraightedge@gmail.com>
2020-03-29 17:09:10 -0700

    Expand development TEMP output

--- lib/faster_factory/cli.rb
+++ lib/faster_factory/cli.rb
@@ -5,9 +5,18 @@ module FasterFactory
     class << self
       def start cli_args
         options = FasterFactory::Options.parse cli_args
-        # commands = FasterFactory::Commands.parse(options)
+        command = [] # FasterFactory::Commands.parse cli_args
+        files   = [] # FasterFactory::Files.parse cli_args
 
-        puts options.inspect
+        puts "Options:"
+        puts "  #{options.inspect}"
+        puts
+        puts "Command:"
+        puts "  #{command.inspect}"
+        puts
+        puts "Files:"
+        puts "  #{files.inspect}"
+        puts
       end
     end
   end

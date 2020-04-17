7cca24bfa0887c97eb83ec5d3b914edfb6875086
Shane Becker <veganstraightedge@gmail.com>
2020-04-17 15:47:15 -0700

    Add FasterFactory::File.new

--- spec/faster_factory/file_spec.rb
+++ spec/faster_factory/file_spec.rb
@@ -1,4 +1,7 @@
 RSpec.describe FasterFactory::File do
-  it "exists" do
+  it "requires an initialization argument" do
+    file = FasterFactory::File.new "path/to/file.rb"
+
+    expect(file).to_not be_nil
   end
 end

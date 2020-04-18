64402dc5f0a7c10123be76f7af078247d47817d5
Shane Becker <veganstraightedge@gmail.com>
2020-04-17 17:53:54 -0700

    Add FasterFactory::TestFile.absolute_path

--- lib/faster_factory/test_file.rb
+++ lib/faster_factory/test_file.rb
@@ -1,18 +1,22 @@
 module FasterFactory
   class TestFile
     def initialize file_path
-      @file_path    = file_path
-      @file_handler = read_file
+      @file_path = file_path
+      clean_file_path!
     end
 
-    def path
-      @file_handler
+    def absolute_path
+      [Dir.pwd, @file_path].join '/'
     end
 
     private
 
     def read_file
       ::File.read @file_path
+    end
+
+    def clean_file_path!
+      @file_path = @file_path[1..-1] if @file_path.start_with? '/'
     end
   end
 end
--- spec/faster_factory/test_file_spec.rb
+++ spec/faster_factory/test_file_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe FasterFactory::TestFile do
     it "calculates its absolute file path" do
       example_spec_file = FasterFactory::TestFile.new "spec/examples/example_spec_file.rb"
 
-      expect(example_spec_file.absolute_path).to eq "spec/examples/example_spec_file.rb"
+      expect(example_spec_file.absolute_path).to eq "#{Dir.pwd}/spec/examples/example_spec_file.rb"
     end
   end
 end

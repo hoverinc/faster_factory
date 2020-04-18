b0e2bc2ccdd04d5a759e85707127c18bc3e604df
Shane Becker <veganstraightedge@gmail.com>
2020-04-17 20:18:29 -0700

    Add from/to init args for CommitMessage.new

--- lib/faster_factory/commit_message.rb
+++ lib/faster_factory/commit_message.rb
@@ -1,9 +1,15 @@
 module FasterFactory
   class CommitMessage
-    def initialize path:, line_number:, template: nil
+    def initialize from:, to:, path:, line_number:, template: nil
+      @from        = from
+      @to          = to
       @path        = path
       @line_number = line_number
       @template    = template
+    end
+
+    def content
+      "[TCR] Replace .build with .build_stubbed in #{@path}:#{@line_number}"
     end
   end
 end
--- spec/faster_factory/commit_message_spec.rb
+++ spec/faster_factory/commit_message_spec.rb
@@ -1,13 +1,27 @@
 RSpec.describe FasterFactory::CommitMessage do
   describe "#initialize" do
+    it "requires initialization arguments: path line_number from to" do
+      commit_message = FasterFactory::CommitMessage.new from: 'create', to: 'build', path: "path/to/file.rb", line_number: 37
+      expect(commit_message).not_to be nil
+    end
+
     it "requires 'path' and 'line_number' initialization arguments" do
-      commit_message = FasterFactory::CommitMessage.new path: "path/to/file.rb", line_number: 37
+      commit_message = FasterFactory::CommitMessage.new from: 'create', to: 'build', path: "path/to/file.rb", line_number: 37
       expect(commit_message).not_to be nil
     end
 
     it "allows a 'template' initialization argument" do
-      commit_message = FasterFactory::CommitMessage.new path: "path/to/file.rb", line_number: 37, template: "Lorem ipsum"
+      commit_message = FasterFactory::CommitMessage.new from: 'create', to: 'build', path: "path/to/file.rb", line_number: 37, template: "Lorem ipsum"
       expect(commit_message).not_to be nil
+    end
+  end
+
+  describe "#content" do
+    context "with no 'template' argument" do
+      it "returns a default value" do
+        commit_message = FasterFactory::CommitMessage.new from: 'create', to: 'build', path: "path/to/file.rb", line_number: 37
+        expect(commit_message.content).not_to be "[TCR] Replace .build with .build_stubbed in path/to/file.rb:37"
+      end
     end
   end
 end

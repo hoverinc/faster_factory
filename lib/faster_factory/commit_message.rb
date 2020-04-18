48d2e4bdd773188121fd969a04bb28a4c63116fe
Shane Becker <veganstraightedge@gmail.com>
2020-04-17 20:09:14 -0700

    Add optional 'content' init arg to CommitMessage

--- lib/faster_factory/commit_message.rb
+++ lib/faster_factory/commit_message.rb
@@ -1,8 +1,9 @@
 module FasterFactory
   class CommitMessage
-    def initialize path:, line_number:
-      @path = path
+    def initialize path:, line_number:, content: nil
+      @path        = path
       @line_number = line_number
+      @content     = content
     end
   end
 end
--- spec/faster_factory/commit_message_spec.rb
+++ spec/faster_factory/commit_message_spec.rb
@@ -1,6 +1,13 @@
 RSpec.describe FasterFactory::CommitMessage do
-  it "requires 'path' and 'line_number' initialization arguments" do
-    commit_message = FasterFactory::CommitMessage.new path: "path/to/file.rb", line_number: 37
-    expect(commit_message).not_to be nil
+  describe "#initialize" do
+    it "requires 'path' and 'line_number' initialization arguments" do
+      commit_message = FasterFactory::CommitMessage.new path: "path/to/file.rb", line_number: 37
+      expect(commit_message).not_to be nil
+    end
+
+    it "allows a 'content' initialization argument" do
+      commit_message = FasterFactory::CommitMessage.new path: "path/to/file.rb", line_number: 37, content: "Lorem ipsum"
+      expect(commit_message).not_to be nil
+    end
   end
 end
